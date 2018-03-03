26f07e7097475bd21781d3c1d9346eac245154e5
temma.io <eifan.devs@gmail.com>
2018-03-04 03:13:23 +0900

    Rootコンポーネント作成

---
+++ src/components/Root.vue
@@ -0,0 +1,33 @@
+<template>
+  <div><p v-if="isVisible">{{ message }}</p></div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      message: 'hello'
+    }
+  },
+  computed: {
+    isVisible: function() {
+      if (this.message) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// langを指定すると、scssやsassでかける。その時は、scss-loaderが必要になる
+// scoped をつけると、そのスタイルはこのファイル内にとどまります。
+div {
+  text-align: center;
+  p {
+    color: red;
+  }
+}
+</style>
