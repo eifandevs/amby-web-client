61130da53343ff8464f535001459a41d57fb7358
temma.io <eifan.devs@gmail.com>
2018-03-04 21:32:38 +0900

    リスト表示を実装

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,29 +1,46 @@
 <template>
-  <div><p v-if="isVisible">{{ message }}</p></div>
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="center">Lists</div>
+    </v-ons-toolbar>
+
+    <v-ons-list>
+      <v-ons-list-header>Thumbnails and titles</v-ons-list-header>
+      <v-ons-list-item>
+        <div class="left">
+          <img class="list-item__thumbnail" src="http://placekitten.com/g/40/40">
+        </div>
+        <div class="center">
+          <span class="list-item__title">Cutest kitty</span><span class="list-item__subtitle">On the Internet</span>
+        </div>
+      </v-ons-list-item>
+
+      <v-ons-list-header>Thumbnails and titles2</v-ons-list-header>
+      <v-ons-list-item>
+        <div class="left">
+          <img class="list-item__thumbnail" src="http://placekitten.com/g/40/40">
+        </div>
+        <div class="center">
+          <span class="list-item__title">Cutest kitty</span><span class="list-item__subtitle">On the Internet</span>
+        </div>
+      </v-ons-list-item>
+    </v-ons-list>
+  </v-ons-page>
 </template>
 
 <script>
+
 export default {
   data: function () {
     return {
-      message: 'hello'
     }
   },
+  methods: {
+  },
   computed: {
-    isVisible: function () {
-      if (this.message) {
-        return true
-      } else {
-        return false
-      }
-    }
   }
 }
 </script>
 
 <style scoped>
-div p {
-  text-align: center;
-  color: red;
-}
 </style>
